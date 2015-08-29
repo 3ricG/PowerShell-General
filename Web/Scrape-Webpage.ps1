84bc98f6efe11ec913f55c84f93b64d6a2f088e2
Eric Gross <egross73@gmail.com>
2015-08-29 17:02:50 -0400

    Initial Commit
    Scrape URLs from a web page. Extremely limited, and should probably be
    expanded...

---
+++ Web/Scrape-Webpage.ps1
@@ -0,0 +1,43 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $URL,
+    [ValidateSet("GetUrls","GetImages")]
+    [string] $Action = "GetUrls"
+)
+
+$objWebClient = New-Object -TypeName System.Net.WebClient
+$arrContent = $objWebClient.DownloadString($URL) -split "`n"
+
+switch($Action) {
+
+    "GetUrls" {
+
+        $arrUrls = @()
+
+        foreach($line in $arrContent) {
+        
+            if( $line -match '.*<a\shref="(.*"\s*>)(.*</a>|\s*/\s*>\s*)' ) {
+
+                $strBaseUrl = $Matches[1] -replace '">$'
+                if($strBaseUrl -notmatch "^http|^https|^www|^\w+\.") {
+
+                    $strFullURL = "$($URL)/$($strBaseUrl)"
+
+                } else {
+
+                    $strFullURL = $strBaseUrl
+
+                }
+
+                $arrUrls += $strFullURL
+
+            }
+
+        }
+
+    }
+
+}
+
+$arrUrls
