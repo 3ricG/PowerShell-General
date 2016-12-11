fa13459bf8128da02ec04c49581147f162d08538
Eric Gross <egross73@gmail.com>
2016-12-11 13:12:07 -0500

    Initial Commits
    Vomit-Rainbow is an old function I've just had in my $PROFILE. I just
    made Get-Paccy based on
    https://github.com/vosh/scripts/blob/master/pacman

---
+++ Fun/Vomit-Rainbow.ps1
@@ -0,0 +1,14 @@
+﻿Function Vomit-Rainbow {
+    param(
+        $StringVal="BLAH",
+        $Seizure
+    )
+
+    if($Seizure) {
+        $StringVal = @("`n"*100)
+    }
+
+    while($true) {
+        Write-Host -Fore $(Get-Random -Min 0 -Max 15) -Back $(Get-Random -Min 0 -Max 15) "$StringVal" -NoNewline
+    }
+}
